70f8cd5c8d52bd2c34d2806cd29a46767f98f0a2
luiseduardo.silva.contractor <luiseduardo.silva.contractor@bbva.com>
2022-04-21 13:16:21 -0500

    Dummy Test

---
+++ test/spiderman.test.js
@@ -0,0 +1,8 @@
+// Testing Spiderman
+
+describe('Test Suite Dummy Description', () => {
+  test('Case 1 Dummy Description', () => {
+    const result = 1 + 2
+    expect(result).toBe(3) 
+  });
+})
