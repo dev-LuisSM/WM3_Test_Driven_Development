42edb371e1a950a9580f1fece9a7daf75f7d1011
luiseduardo.silva.contractor <luiseduardo.silva.contractor@bbva.com>
2022-04-21 13:26:21 -0500

    add : spiderman class and test

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -1,8 +1,16 @@
 // Testing Spiderman
+const Spiderman = require('./../app/spiderman')
 
-describe('Test Suite Dummy Description', () => {
-  test('Case 1 Dummy Description', () => {
-    const result = 1 + 2
-    expect(result).toBe(3) 
+describe('Unit Test for Spiderman Class', () => {
+  test('1) Create a spiderman object', () => {
+    // Instancia del objeto como normalmente se hace
+    const andrew = new Spiderman("Amazing Spiderman", 31, "Andrew Garfield", 3, "Sony")
+
+    // Se valida que el código funcione como debería
+    expect(andrew.name).toBe('Amazing Spiderman')
+    expect(andrew.age).toBe(31)
+    expect(andrew.actor).toBe('Andrew Garfield')
+    expect(andrew.movies).toBe(3)
+    expect(andrew.studio).toBe('Sony')
   });
 })
